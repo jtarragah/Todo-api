4f1ec02c1b806171a4723a56bc08da4ef8d830be
Jacinto Tarraga <jtarragah@gmail.com>
2017-01-15 20:50:31 +0100

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
 // GET /todos
-app.get('/todos', function(req, res) {
+app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
-//GET /todos/:id
-app.get('/todos/:id', function(req, res) {
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
@@ -30,24 +30,24 @@ app.get('/todos/:id', function(req, res) {
 	}
 });
 
-//POST /todos
-app.post('/todos', function (req, res){
+// POST /todos
+app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
 
-	body.description = body.description.trim();
+	body.description = body.description.trim();	
 	body.id = todoNextId++;
 
 	todos.push(body);
-
+	
 	res.json(body);
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
@@ -59,6 +59,51 @@ app.delete('/todos/:id', function(req, res) {
 	}
 });
 
-app.listen(PORT, function(){
-	console.log('express listening on port ' + PORT + '!');
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
+
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
